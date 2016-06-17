e544d71cae0b931a541e724f73d1b4b202bc5f34
janizzle <janos.imhof@yetnet.ch>
2016-06-17 08:43:10 +0200

    vds

--- iApp/iApp_New/Database/Queries/Find.php
+++ iApp/iApp_New/Database/Queries/Find.php
@@ -4,7 +4,9 @@
     require('../../Loaders/Autoload.php');
 
     use roobique\Database\Wrappers\Connector;
-
+    use MongoClient;
+    use MongoCollection;
+    
     class Find extends Connector {
       	public function find($statement, $collection) {
           $collection = $this->connect($collection);
