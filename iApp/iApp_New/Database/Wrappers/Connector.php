0faed59f186a9379514bf8290beb0dccb5dbf83a
janizzle <janos.imhof@yetnet.ch>
2016-06-17 09:23:52 +0200

    cdas

--- iApp/iApp_New/Database/Wrappers/Connector.php
+++ iApp/iApp_New/Database/Wrappers/Connector.php
@@ -12,7 +12,7 @@
       abstract protected function find($statement, $collection);
 
       public function connect($collection) {
-        $client = new Client("mongodb://localhost:27017");
+        $client = new MongoDB\Client("mongodb://localhost:27017");
         return $client->selectDatabase('roobique')->selectCollection($collection);
       }
     }
