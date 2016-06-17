8cac9f3c1678b8a6a1daecfbc319901538a9b1de
janizzle <janos.imhof@yetnet.ch>
2016-06-17 14:29:52 +0200

    fsdfsdf

--- iApp/iApp_New/Database/Wrappers/test.php
+++ iApp/iApp_New/Database/Wrappers/test.php
@@ -6,8 +6,4 @@
   $find = new Find();
 
   $test = $find->find(array(), 'Images');
-  json_decode($test, true);
-  foreach($test as $bla ){
-    print_r($bla);
-    echo $bla[0]['_id']->{'$id'};
-  }
+  print_r($test);
