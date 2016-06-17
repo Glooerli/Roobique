9dd840defa8d4e58e2fe635d343c7d2488d7e720
janizzle <janos.imhof@yetnet.ch>
2016-06-17 09:44:16 +0200

    ijdsio

--- iApp/iApp_New/Database/Wrappers/Connector.php
+++ iApp/iApp_New/Database/Wrappers/Connector.php
@@ -12,7 +12,7 @@
       abstract protected function find($statement, $collection);
 
       public function connect($collection) {
-        $client = new MongoDB\Client("mongodb://localhost:27017");
+        $client = new MongoClient("mongodb://localhost:27017");
         return $client->selectDatabase('roobique')->selectCollection($collection);
       }
     }
