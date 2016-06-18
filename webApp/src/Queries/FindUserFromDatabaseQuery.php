000724278fd7a30d51a2e0238563287d44240129
Glooerli <luki.dc@hotmail.com>
2016-06-18 12:04:36 +0200

    test

--- webApp/src/Queries/FindUserFromDatabaseQuery.php
+++ webApp/src/Queries/FindUserFromDatabaseQuery.php
@@ -19,11 +19,11 @@ namespace roobique\Queries {
         private function find($userData)
         {
             $InstaID = $userData['InstaID'];
+            echo $userData['InstaID'];
+            var_dump($userData);
 
-            $connection = $this->connectCollection('Users');
-            $userInformation = $connection->find(array('id' => $InstaID));
-            echo $userInformation['username'];
-            echo $userInformation['_id'];
+//            $connection = $this->connectCollection('Users');
+//            $userInformation = $connection->find(array('id' => $InstaID));
 //            foreach ($userInformation as $doc) {
 //                echo $doc['_id'];
 //                echo $doc['username'];
