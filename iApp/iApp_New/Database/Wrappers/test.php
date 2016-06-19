ace44ab5ff55ca2b8132925047e40a8ccb17d355
janizzle <janosimhof@Janoss-MacBook-Pro.local>
2016-06-19 17:54:22 +0200

    hhjgi

--- iApp/iApp_New/Database/Wrappers/test.php
+++ iApp/iApp_New/Database/Wrappers/test.php
@@ -8,9 +8,16 @@
   $test = $find->find(array(), 'Images');
   echo (string)$test[0]['_id']; */
 
-  use roobique\Database\Queries\Random;
+  /* use roobique\Database\Queries\Random;
 
   $random = new Random();
 
   $test = $random->random('Images');
+  echo $test; */
+
+  use roobique\Database\Queries\Count;
+
+  $count = new Count();
+
+  $test = $count->count('Images');
   echo $test;
