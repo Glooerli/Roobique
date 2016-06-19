ed17611167e982970df98bf882b633dd67059411
janizzle <janosimhof@Janoss-MacBook-Pro.local>
2016-06-19 23:01:09 +0200

    fwefew

--- iApp/iApp_New/Database/Wrappers/test.php
+++ iApp/iApp_New/Database/Wrappers/test.php
@@ -1,21 +1,21 @@
 <?php
   require('../../Loaders/Autoload.php');
 
-  /* use roobique\Database\Queries\Find;
+  use roobique\Database\Queries\Find;
 
   $find = new Find();
 
   $test = $find->find(array(), 'Images');
-  echo (string)$test[0]['_id']; */
+  print_r($test);
 
-  use roobique\Database\Queries\Random;
+  /* use roobique\Database\Queries\Random;
 
   $random = new Random();
 
   $test = $random->random('Images');
   echo $test;
 
-  /* use roobique\Database\Queries\Count;
+  use roobique\Database\Queries\Count;
 
   $count = new Count();
 
