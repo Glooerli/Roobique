0ca3c50789665f73745f76ec6776cfdc4c81af9e
janizzle <janosimhof@Janoss-MacBook-Pro.local>
2016-06-19 19:26:30 +0200

    jhhkjjk

--- iApp/iApp_New/Database/Queries/Random.php
+++ iApp/iApp_New/Database/Queries/Random.php
@@ -4,7 +4,7 @@
     require('/var/www/roobique/vendor/autoload.php');
 
     use roobique\Database\Wrappers\Connector;
-    use roobique\Database\Wrappers\Count;
+    use roobique\Database\Queries\Count;
 
     class Random extends Count {
       	public function random($collection) {
