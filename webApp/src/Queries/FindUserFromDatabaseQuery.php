eaa6d17790e8934ffa11606a9c0796d03443c27f
Lukas Gloor <luki.dc@hotmail.com>
2016-06-19 16:18:35 +0200

    sregt mi langsam uf

--- webApp/src/Queries/FindUserFromDatabaseQuery.php
+++ webApp/src/Queries/FindUserFromDatabaseQuery.php
@@ -20,7 +20,7 @@ namespace roobique\Queries {
         {
             $connection = $this->connectCollection('Users');
             if (isset($userData['user']['username'])) {
-                return $connection->find(array('user.username' => $userData['user']['username']));
+                return $connection->find(array('username' => $userData['user']['username'], 'username'));
             }
         }
     }
