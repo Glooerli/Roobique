07002b12e0d20e66122e19ad9f4adf71678d4dfe
janizzle <janos.imhof@yetnet.ch>
2016-06-20 14:19:38 +0200

    xwq

--- iApp/iApp_New/Database/Wrappers/test.php
+++ iApp/iApp_New/Database/Wrappers/test.php
@@ -1,20 +1,21 @@
 <?php
   require('../../Loaders/Autoload.php');
 
-  use roobique\Database\Queries\Find;
+/*  use roobique\Database\Queries\Find;
 
   $find = new Find();
 
   $test = $find->find(array(), 'Images');
-  print_r($test);
+  print_r($test); */
 
-  /* use roobique\Database\Queries\Random;
+  use roobique\Database\Queries\Random;
 
   $random = new Random();
 
   $test = $random->random('Images');
   echo $test;
-
+  
+/*
   use roobique\Database\Queries\Count;
 
   $count = new Count();
